e571a3791f5281e3be63041e63006ba39daf35b6
Travo100 <travo100@gmail.com>
2018-02-03 08:40:32 -0800

    Chihuahua game v1 working

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,16 +2,33 @@ import React, {Component} from "react";
 import Header from "./../Header";
 import Footer from "./../Footer";
 
+import cards from "./../../cards.json"
+
 class Game extends Component {
     state = {
+        chihuahuas : cards,
+        randomChihuahuaId: Math.floor(Math.random() * cards.length) + 1  
+    }
 
+    pickedChihuahua = (chihuahua) => {
+        console.log(this.state.randomChihuahuaId);
+        if(this.state.randomChihuahuaId === chihuahua.id) {
+            alert(`Hooray! You and ${chihuahua.name} picked each other!`);
+        } else { 
+            alert(`I guess it wasn't meant to be! You picked ${chihuahua.name}`);
+        }
+        
     }
 
     render() {
         return (
-            <div class="container">
+            <div className="container">
                 <Header />
-
+                    {this.state.chihuahuas.map(chihuahua => 
+                        <button onClick={() => this.pickedChihuahua(chihuahua)} key={chihuahua.id}>
+                            <img src={chihuahua.image} alt={chihuahua.name}/>
+                        </button>
+                    )}
                 <Footer />
             </div>
         )
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,8 @@ import "./Header.css";
 
 const Header = props => (
   <header>
-    <h1>Welcome to the crystal game!</h1>
+    <h1>Welcome to the chihuahua game!</h1>
+    <p>In this game you pick a chihuahua, hopefully they pick you too to be their owner!</p>
   </header>
 );
 
